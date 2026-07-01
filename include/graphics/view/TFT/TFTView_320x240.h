7b8938b0cc16a8adeacd74843a6339dc7ab274d7
Sergey Krashevich <svk@svk.su>
2026-07-01 04:18:53 +0300

    feat(TFTView): add new node management methods - implement methods to add, update, and remove nodes for better node handling - enhance node updates with signal strength and connection status

--- include/graphics/view/TFT/TFTView_320x240.h
+++ include/graphics/view/TFT/TFTView_320x240.h
@@ -11,6 +11,44 @@
 class TFTView_320x240 : public TFTView_Common
 {
   public:
+    void addOrUpdateNode(uint32_t nodeNum, uint8_t channel, uint32_t lastHeard, const meshtastic_User &cfg) override;
+    void addNode(uint32_t nodeNum, uint8_t channel, const char *userShort, const char *userLong, uint32_t lastHeard, eRole role,
+                 bool hasKey, bool unmessagable) override;
+    void updateNode(uint32_t nodeNum, uint8_t channel, const meshtastic_User &cfg) override;
+    void updateMetrics(uint32_t nodeNum, uint32_t bat_level, float voltage, float chUtil, float airUtil) override;
+    void updateSignalStrength(uint32_t nodeNum, int32_t rssi, float snr) override;
+    void updateHopsAway(uint32_t nodeNum, uint8_t hopsAway) override;
+    void updateConnectionStatus(const meshtastic_DeviceConnectionStatus &status) override;
+    void removeNode(uint32_t nodeNum) override;
+
+    void updateChannelConfig(const meshtastic_Channel &ch) override;
+    void updateTime(uint32_t time) override;
+
+    void newMessage(uint32_t from, uint32_t to, uint8_t ch, const char *msg, uint32_t &msgtime, bool restore = true) override;
+    void restoreMessage(const LogMessage &msg) override;
+
+  protected:
+    void setGroupFocus(lv_obj_t *panel);
+
+    void purgeNode(uint32_t nodeNum);
+    bool applyNodesFilter(uint32_t nodeNum, bool reset = false);
+    void setNodeImage(uint32_t nodeNum, eRole role, bool unmessagable, lv_obj_t *img);
+    void updateNodesStatus(void);
+
+    lv_obj_t *newMessageContainer(uint32_t from, uint32_t to, uint8_t ch);
+    void newMessage(uint32_t nodeNum, lv_obj_t *container, uint8_t channel, const char *msg);
+    void addChat(uint32_t from, uint32_t to, uint8_t ch);
+    void showMessages(uint8_t channel);
+    void showMessages(uint32_t nodeNum);
+    void highlightChat(uint32_t from, uint32_t to, uint8_t ch);
+    void updateActiveChats(void);
+    void showMessagePopup(uint32_t from, uint32_t to, uint8_t ch, const char *name);
+
+    void updateTime(void);
+    void updateSignalStrength(int32_t rssi, float snr);
+
+    void backup(uint32_t option);
+    void restore(uint32_t option);
 
   private:
     // view creation only via ViewFactory
